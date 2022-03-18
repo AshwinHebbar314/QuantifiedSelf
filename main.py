0c1f866b463da2f679fa47384015fbb1e534e30d
AshwinHebbar314 <ashwinhebbar2k02@gmail.com>
2022-03-18 21:11:56 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from application.database import db 
+from application.models import *
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'sicrit'
+db = SQLAlchemy(app)
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.app_context().push()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+
+from application.methods import *
+
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     return r
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run()
