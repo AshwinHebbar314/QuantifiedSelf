6c2a706c99b50cbb080ee249456f80d4f209bf2d
AshwinHebbar314 <ashwinhebbar2k02@gmail.com>
2022-03-21 19:37:43 +0530

    make old mathods file and new one without commented code

---
+++ methods.py
@@ -0,0 +1,16 @@
+from flask_login import login_user, logout_user, current_user, login_required
+from flask import current_app as app, render_template, request, redirect, flash
+from application.models import *
+from datetime import datetime
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+
+
+
+from controllers.appfunc import *
+from controllers.userops import *
+from controllers.trackerops import *
+from controllers.logops import *
+from controllers.miscmethods import *
